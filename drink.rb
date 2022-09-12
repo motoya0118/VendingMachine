d47cdd9dffaf22cb4d09df0f939eecf79eb258fa
motoya <motoyao0118@gmail.com>
2022-09-12 17:42:51 +0900

    first commit

---
+++ drink.rb
@@ -0,0 +1,18 @@
+module Drink
+  def initialize_drink
+    {
+      "cola"=>{
+        price:120,
+        num:5
+      },
+      "redbull"=>{
+        price:200,
+        num:5
+      },
+      "water"=>{
+        price:100,
+        num:5
+      }
+    }
+  end
+end
--- vending_machine.rb
+++ vending_machine.rb
@@ -1,37 +1,87 @@
-class VendingMachine
-  # ステップ０　お金の投入と払い戻しの例コード
-  # ステップ１　扱えないお金の例コード
+require_relative '/Users/motoya/study/vending_machine/drink.rb'
 
-  # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
+class VendingMachine
+  include Drink
+  
   MONEY = [10, 50, 100, 500, 1000].freeze
 
-  # （自動販売機に投入された金額をインスタンス変数の @slot_money に代入する）
   def initialize
-    # 最初の自動販売機に入っている金額は0円
     @slot_money = 0
+    @sum = 0
+    @drinks = initialize_drink
   end
 
-  # 投入金額の総計を取得できる。
   def current_slot_money
-    # 自動販売機に入っているお金を表示する
     @slot_money
   end
 
-  # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
-  # 投入は複数回できる。
-  def slot_money(money)
-    # 想定外のもの（１円玉や５円玉。千円札以外のお札、そもそもお金じゃないもの（数字以外のもの）など）
-    # が投入された場合は、投入金額に加算せず、それをそのまま釣り銭としてユーザに出力する。
-    return false unless MONEY.include?(money)
-    # 自動販売機にお金を入れる
-    @slot_money += money
+  def current_drinks
+    @drinks
   end
 
-  # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。
+  def current_sum
+    @sum
+  end
+
+  def slot_money(money)
+    return false unless MONEY.include?(money)
+    @slot_money += money
+  end
+ 
   def return_money
-    # 返すお金の金額を表示する
-    puts @slot_money
-    # 自動販売機に入っているお金を0円に戻す
+    puts "おつり：#{@slot_money}"
     @slot_money = 0
   end
+  
+  def restock(drink,price,num)
+    unless price > 0
+      puts 'priceは正の整数にして下さい'
+      return nil
+    end
+    unless num > 0
+      puts '在庫は正の整数にして下さい'
+      return nil
+    end
+    obj = {price:price,num:num}
+    if @drinks.include?(drink)
+      @drinks[drink][:num] = @drinks[drink][:num].to_i + num
+    else
+      @drinks[drink] = obj
+    end
+    puts @drinks
+  end
+
+  def can_buy_drinks
+    can_buy_drinks = []
+    @drinks.each do |drink,params|
+      if params[:num] > 0 && @slot_money >= params[:price]
+        can_buy_drinks << {drink => params[:price] }
+      end
+    end
+    can_buy_drinks
+  end
+  
+  def buy
+    drinks_list = can_buy_drinks
+    puts "購入可能なドリンクリスト"
+    drinks_list.each_with_index {|drink,n| puts "#{n}:#{drink.keys[0]},値段:#{drink.values[0]}"}
+    puts "数字を入力してください"
+    user_select = gets.to_i
+    if user_select < drinks_list.length
+      drink_name = drinks_list[user_select].keys[0]
+      price = drinks_list[user_select].values[0]
+      @slot_money -=  price
+      @drinks[drink_name][:num] -= 1
+      @sum += price
+      puts "#{drink_name}を購入しました"
+      return_money
+    else
+      puts "選択したドリンクはありません"
+   end
+  end
 end
+
+
+
+
+
